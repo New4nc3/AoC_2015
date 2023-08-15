2057afd7df84341e0a6073f1a54546b4456efdcb
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2023-08-15 13:52:50 +0200

    Initial commit. Add Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Day1;
+
+class Program
+{
+    private const char _upFloor = '(';
+    private const char _downFloor = ')';
+
+    static void Main(string[] args)
+    {
+        var inputFileName = args.Length == 0 ? "test.txt" : args[0];
+        string data;
+
+        using (var streamReader = new StreamReader(inputFileName))
+            data = streamReader.ReadToEnd();
+
+        var currentFloor = 0;
+        var firstBasementOccurenceIndex = -1;
+        var length = data.Length;
+
+        for (var i = 0; i < length; ++i)
+        {
+            var floor = data[i];
+
+            if (floor == _upFloor)
+                ++currentFloor;
+            else if (floor == _downFloor)
+                --currentFloor;
+            else
+                throw new ArgumentException($"Unknown floor '{floor}', check input data");
+
+            if (currentFloor == -1 && firstBasementOccurenceIndex == -1)
+                firstBasementOccurenceIndex = i + 1;
+        }
+
+        Console.WriteLine($"Part 1. Santa is on {currentFloor} floor");
+        Console.WriteLine($"Part 2. First basement occurence is on position {firstBasementOccurenceIndex}");
+    }
+}
