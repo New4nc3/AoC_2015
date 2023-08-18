fb7cd8113d00991a0611f4cfc96cc964c591bfac
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2023-08-18 18:44:26 +0200

    Add Day 8

---
+++ Day8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputFileName = args.Length == 0 ? "test.txt" : args[0];
+        string[] inputData;
+
+        using (var streamReader = new StreamReader(inputFileName))
+            inputData = streamReader.ReadToEnd().Split("\r\n");
+
+        var charsInCode = 0;
+        var charsInMemory = 0;
+        var encodedCharsInCode = 0;
+        StringBuilder stringBuilder;
+
+        foreach (var input in inputData)
+        {
+            charsInCode += input.Length;
+            charsInMemory += Regex.Unescape(input[1..^1]).Length;
+            encodedCharsInCode += Encode(input).Length;
+        }
+
+        Console.WriteLine($"Part 1. Difference is: {charsInCode - charsInMemory} characters");
+        Console.WriteLine($"Part 2. Now difference is: {encodedCharsInCode - charsInCode} characters");
+
+        string Encode(string str)
+        {
+            stringBuilder = new StringBuilder("\"");
+
+            foreach (var character in str)
+            {
+                switch (character)
+                {
+                    case '\"':
+                    case '\\':
+                        stringBuilder.Append('\\');
+                        break;
+                }
+
+                stringBuilder.Append(character);
+            }
+
+            stringBuilder.Append('\"');
+            return stringBuilder.ToString();
+        }
+    }
+}
