68e551ba35e1b9e05a70f91b5c76f3b463344862
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2023-08-24 12:28:30 +0200

    Add Day 15

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -29,11 +29,56 @@ class Program
             ingredients.Add(new Ingredient(name, capacity, durability, flavor, texture, calories));
         }
 
-        foreach (var combination in GenerateCombination(ingredients.Count))
+        var bestCombination = new List<byte>();
+        var bestCaloriesCombination = new List<byte>();
+        var bestScore = 0;
+        var bestScoreForCalories = 0;
+        var ingredientsCount = ingredients.Count;
+
+        foreach (var combination in GenerateCombination(ingredientsCount))
         {
-            Console.Write($"{string.Join(" ", combination)}\t");
+            int totalCapacity = 0, totalDurability = 0, totalFlavor = 0, totalTexture = 0, totalCalories = 0;
+
+            for (var i = 0; i < ingredientsCount; ++i)
+            {
+                var combinationAmount = combination[i];
+                var ingredient = ingredients[i];
+
+                totalCapacity += combinationAmount * ingredient.Capacity;
+                totalDurability += combinationAmount * ingredient.Durability;
+                totalFlavor += combinationAmount * ingredient.Flavor;
+                totalTexture += combinationAmount * ingredient.Texture;
+                totalCalories += combinationAmount * ingredient.Calories;
+            }
+
+            if (IsAnyNegative(totalCapacity, totalDurability, totalFlavor, totalTexture))
+                continue;
+
+            var tempScore = totalCapacity * totalDurability * totalFlavor * totalTexture;
+
+            if (tempScore > bestScore)
+            {
+                bestScore = tempScore;
+                bestCombination = new List<byte>(combination);
+            }
+
+            if (totalCalories == 500 && tempScore > bestScoreForCalories)
+            {
+                bestScoreForCalories = tempScore;
+                bestCaloriesCombination = new List<byte>(combination);
+            }
         }
+
+        var ingredientNames = ingredients.Select(x => x.Name).ToList();
+
+        Console.WriteLine($"Part 1. Best score is: {bestScore}, created by next amounts: {string.Join(", ", bestCombination)}" + 
+            $" of ingredients {string.Join(", ", ingredientNames)} respectively");
+        Console.WriteLine($"Part 2. Now, the best score with achieving 500 calories is: {bestScoreForCalories}, created by " + 
+            $"{string.Join(", ", bestCaloriesCombination)} of {string.Join(", ", ingredientNames)} respectively");
     }
+
+    static bool IsAnyNegative(params int[] properties) =>
+        properties.Any(p => p < 0);
 
     static IEnumerable<IList<byte>> GenerateCombination(int count)
     {
@@ -43,7 +88,8 @@ class Program
             combinations.Add(0);
 
         var lastIndex = count - 1;
-        int outOfRangeIndex = 0;
+        int outOfRangeIndex;
+        int tempSum;
 
         while (true)
         {
@@ -53,13 +99,26 @@ class Program
             {
                 if (outOfRangeIndex == 0)
                     yield break;
-                
+
                 combinations[outOfRangeIndex] = 0;
                 ++combinations[outOfRangeIndex - 1];
             }
 
-            if (combinations.Sum(x => x) == 100)
-                yield return combinations;
+            tempSum = 0;
+            for (var i = 0; i < lastIndex; ++i)
+                tempSum += combinations[i];
+
+            if (tempSum > 100)
+            {
+                combinations[lastIndex - 1] = 100;
+                continue;
+            }
+
+            combinations[lastIndex] = (byte)(100 - tempSum);
+
+            yield return combinations;
+
+            combinations[lastIndex] = 100;
         }
     }
 }
