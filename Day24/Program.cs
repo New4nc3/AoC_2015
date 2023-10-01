66a34c6e4a84df866410c54ed1ddfde21110cff2
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2023-10-01 12:09:32 +0200

    Add Day 24 placeholder

---
+++ Day24/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day24;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputFileName = args.Length == 0 ? "test.txt" : args[0];
+        var packages = File.ReadAllLines(inputFileName).Select(int.Parse).ToList();
+
+        Console.WriteLine($"Total packages: {packages.Count}. Total weight: {packages.Sum()}");
+    }
+}
