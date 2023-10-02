471aa9869452496006d39255bf9a8275e403ead4
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2023-10-02 19:24:26 +0200

    Day 24 work in progress

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -7,7 +7,7 @@ class Program
         var inputFileName = args.Length == 0 ? "test.txt" : args[0];
         var packages = File.ReadAllLines(inputFileName).Select(int.Parse).OrderByDescending(x => x).ToList();
         var sum = packages.Sum() / 3;
-        var group1 = new List<HashSet<int>>();
+        var group1 = new List<List<int>>();
 
         GenerateGroups(packages, sum, 0, new List<int>(), group1);
 
@@ -18,12 +18,43 @@ class Program
         Console.WriteLine($"Count of possible combinations to get {sum}: {group1.Count}");
     }
 
-    static void GenerateGroups(List<int> packages, int desiredWeight, int sum, List<int> tempGroup, List<HashSet<int>> groups)
+    static void AddIfNoContains(List<int> group, List<List<int>> groups)
+    {
+        var sorted = group.OrderByDescending(x => x).ToList();
+        var count = sorted.Count;
+
+        foreach (var currentGroup in groups)
+        {
+            var currentCount = currentGroup.Count;
+
+            if (currentCount != count)
+                continue;
+
+            var skipToNext = false;
+            
+            for (var i = 0; i < count; ++i)
+            {
+                if (sorted[i] != currentGroup[i])
+                {
+                    skipToNext = true;
+                    break;
+                }
+            }
+
+            if (skipToNext)
+                continue;
+            else
+                return;
+        }
+
+        groups.Add(sorted);
+    }
+
+    static void GenerateGroups(List<int> packages, int desiredWeight, int sum, List<int> tempGroup, List<List<int>> groups)
     {
         if (sum == desiredWeight)
         {
-            tempGroup = tempGroup.OrderByDescending(x => x).ToList();
-            groups.Add(tempGroup.ToHashSet());
+            AddIfNoContains(tempGroup, groups);
         }
         else if (sum < desiredWeight)
         {
