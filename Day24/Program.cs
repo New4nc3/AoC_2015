4ca00c8ec64f58cbaf276f2c6313cecba330406b
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2024-01-06 00:04:18 +0100

    Day 24. Part 2 work in progress

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -1,15 +1,14 @@
-﻿using System.Diagnostics;
-
-namespace Day24;
+﻿namespace Day24;
 
 class ArrangementContainer
 {
     public List<int> First { get; private set; }
-    public List<int> Second { get; private set; }
-    public List<int> Third { get; private set; }
+    public List<int>? Second { get; private set; } = null;
+    public List<int>? Third { get; private set; } = null;
 
     public long QE { get; private set; }
-    public bool Initialized => First != null && Second != null && Third != null;
+
+    public virtual bool Initialized => First != null && Second != null && Third != null;
 
     public ArrangementContainer(IEnumerable<int> firstGroup)
     {
@@ -20,58 +19,117 @@ class ArrangementContainer
         QE = product;
     }
 
-    public void TryInitGroup(IEnumerable<int> group)
+    public virtual bool TryInitGroup(IEnumerable<int> group)
     {
         if (Second == null)
+        {
             Second = new List<int>(group);
-        else if (Third == null)
+            return true;
+        }
+
+        if (Third == null)
+        {
             Third = new List<int>(group);
+            return true;
+        }
+
+        return false;
     }
 
     public override string ToString() =>
         $"({string.Join(", ", First)}); ({string.Join(", ", Second)}); ({string.Join(", ", Third)}) with QE = {QE}";
 }
 
+class ArrangementContainer2 : ArrangementContainer
+{
+    public List<int>? Fourth { get; private set; } = null;
+
+    public ArrangementContainer2(IEnumerable<int> firstGroup) : base(firstGroup) { }
+
+    public override bool TryInitGroup(IEnumerable<int> group)
+    {
+        if (!base.TryInitGroup(group) && Fourth == null)
+        {
+            Fourth = new List<int>(group);
+            return true;
+        }
+
+        return false;
+    }
+}
+
 class Program
 {
-    private static List<List<int>> _uniqueSubgroups = new();
+    private static readonly List<List<int>> _uniqueGroups = new();
+    private static readonly List<List<int>> _uniqueSubgroups = new();
 
     static void Main(string[] args)
     {
         var inputFileName = args.Length == 0 ? "test.txt" : args[0];
         var packages = File.ReadAllLines(inputFileName).Select(int.Parse).OrderByDescending(x => x).ToList();
-        var sum = packages.Sum() / 3;
-        var uniqueGroups = new List<List<int>>();
+        var sumPackages = packages.Sum();
+        var sumPart1 = sumPackages / 3;
+        var sumPart2 = sumPackages / 4;
 
+        Console.WriteLine($"Total packages: {packages.Count}. Total weight: {packages.Sum()}. Required weights: ({sumPart1}; {sumPart2})\n");
+
+        Solve(packages, sumPart1, part1: true);
+        Solve(packages, sumPart2, part1: false);
+    }
+
+    private static void Solve(List<int> packages, int desiredWeight, bool part1)
+    {
         ArrangementContainer? bestArrangement1 = null;
+        ArrangementContainer2? bestArrangement2 = null;
 
-        foreach (var candidate in GenerateCandidatesRecursively(packages, sum, 0, new List<int>()))
+        ArrangementContainer? temp1 = null;
+        ArrangementContainer2? temp2 = null;
+
+        foreach (var candidate in GenerateCandidatesRecursively(packages, desiredWeight, 0, new List<int>()))
         {
-            if (!TryAddToUniqueGroups(candidate, uniqueGroups))
+            if (!TryAddToUniqueGroups(candidate, _uniqueGroups))
                 continue;
 
-            var tempArrangement = new ArrangementContainer(candidate);
+            if (part1)
+            {
+                temp1 = new ArrangementContainer(candidate);
 
-            if (bestArrangement1 != null && (bestArrangement1.First.Count < candidate.Count || bestArrangement1.QE < tempArrangement.QE))
-                continue;
+                if (bestArrangement1 != null && (bestArrangement1.First.Count < candidate.Count || bestArrangement1.QE < temp1.QE))
+                    continue;
+            }
+            else
+            {
+                temp2 = new ArrangementContainer2(candidate);
+
+                if (bestArrangement2 != null && (bestArrangement2.First.Count < candidate.Count || bestArrangement2.QE < temp2.QE))
+                    continue;
+            }
 
             try
             {
-                TryInitArrangementRecursively(packages.Except(candidate).ToList(), sum, 0, new List<int>(), tempArrangement);
+                TryInitArrangementRecursively(packages.Except(candidate).ToList(), desiredWeight, 0, new List<int>(), part1 ? temp1 : temp2);
             }
             catch { }
 
-            if (tempArrangement.Initialized && (bestArrangement1 == null || tempArrangement.QE < bestArrangement1.QE))
+            if (part1)
             {
-                bestArrangement1 = tempArrangement;
-                break;
+                if (temp1.Initialized && (bestArrangement1 == null || temp1.QE < bestArrangement1.QE))
+                {
+                    bestArrangement1 = temp1;
+                    Console.WriteLine($"Part 1. Best arrangement to get {desiredWeight}: {bestArrangement1}");
+                    break;
+                }
+            }
+            else
+            {
+                if (temp2.Initialized && (bestArrangement2 == null || temp2.QE < bestArrangement2.QE))
+                {
+                    bestArrangement2 = temp2;
+                    Console.WriteLine($"Part 2. Best arrangement to get {desiredWeight}: {bestArrangement2}");
+                    break;
+                }
             }
         }
-
-        Console.WriteLine($"Total packages: {packages.Count}. Total weight: {packages.Sum()};");
-
-        if (bestArrangement1 != null)
-            Console.WriteLine($"Best arrangement to get {sum}: {bestArrangement1}");
     }
 
     private static bool TryAddToUniqueGroups(List<int> group, List<List<int>> groups)
@@ -113,15 +171,18 @@ class Program
         {
             container.TryInitGroup(currentGroup);
 
-            if (remainingPackages.Sum(x => x) == desiredWeight)
+            var sumOfRemaining = remainingPackages.Sum(x => x);
+
+            if (sumOfRemaining == desiredWeight)
             {
                 container.TryInitGroup(remainingPackages);
                 TryAddToUniqueGroups(remainingPackages, _uniqueSubgroups);
                 throw new NotImplementedException();
             }
-
-            // if (remainingPackages.Count > 0)
-            //     TryInitArrangementRecursively(remainingPackages, desiredWeight, 0, new List<int>(), container);
+            else if (sumOfRemaining > desiredWeight)
+            {
+                TryInitArrangementRecursively(remainingPackages, desiredWeight, 0, new List<int>(), container);
+            }
         }
         else if (currentWeight < desiredWeight)
         {
