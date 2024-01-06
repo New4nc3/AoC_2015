0f710a58e24190c4aa51621ea00832329fed3a78
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2024-01-06 12:47:40 +0100

    Day 25. Done

--- Day25/Program.cs
+++ Day25/Program.cs
@@ -5,7 +5,7 @@ namespace Day25;
 partial class Program
 {
     private const string _digitsPattern = @"\d+";
-    
+
     [GeneratedRegex(_digitsPattern)]
     private static partial Regex NumbersRegex();
 
@@ -28,7 +28,35 @@ partial class Program
 
         var destRow = inputNumbers[0] - 1;
         var destCol = inputNumbers[1] - 1;
+        var diagonal = 0;
+        var isTest = inputFileName == "test.txt";
+        long currentValue = isTest ? 1 : 20151125;
+        long multiplier = 252533;
+        long divider = 33554393;
+        int tempRowIdx = 0, tempColIdx = 0;
 
-        Console.WriteLine(string.Join(", ", inputNumbers) + $"\n{destRow}; {destCol}");
+        while (tempRowIdx != destRow || tempColIdx != destCol)
+        {
+            ++diagonal;
+            tempRowIdx = diagonal;
+            tempColIdx = 0;
+
+            do
+            {
+                currentValue = isTest ? currentValue + 1 : currentValue * multiplier % divider;
+
+                if (tempRowIdx == destRow && tempColIdx == destCol)
+                    break;
+
+                if (tempRowIdx - 1 >= 0)
+                {
+                    --tempRowIdx;
+                    ++tempColIdx;
+                }
+                else break;
+            } while (true);
+        }
+
+        Console.WriteLine($"Part 1. Value on row {inputNumbers[0]} and column {inputNumbers[1]} was found on diagonal #{diagonal + 1} and equals {currentValue}");
     }
 }
