54471dfe3cd794d73e4108ae0977b3577061a232
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2024-01-06 11:57:59 +0100

    Day 25. Add read input

--- Day25/Program.cs
+++ Day25/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Day25;
+﻿using System.Text.RegularExpressions;
 
-class Program
+namespace Day25;
+
+partial class Program
 {
+    private const string _digitsPattern = @"\d+";
+    
+    [GeneratedRegex(_digitsPattern)]
+    private static partial Regex NumbersRegex();
+
     static void Main(string[] args)
     {
         var inputFileName = args.Length == 0 ? "test.txt" : args[0];
-        Console.WriteLine("test");
+        string? rawData;
+
+        using (var streamReader = new StreamReader(inputFileName))
+            rawData = streamReader.ReadLine();
+
+        if (string.IsNullOrEmpty(rawData))
+            throw new ArgumentException("Check input file and try again");
+
+        var inputNumbers = new int[2];
+        var i = 0;
+
+        foreach (Match match in NumbersRegex().Matches(rawData).Cast<Match>())
+            inputNumbers[i++] = Convert.ToInt32(match.Value);
+
+        var destRow = inputNumbers[0] - 1;
+        var destCol = inputNumbers[1] - 1;
+
+        Console.WriteLine(string.Join(", ", inputNumbers) + $"\n{destRow}; {destCol}");
     }
 }
