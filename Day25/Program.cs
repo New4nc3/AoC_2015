bb348a87b83bff1b4612c8460750c76db6ff2316
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2024-01-06 11:42:57 +0100

    Add Day 25 placeholder

---
+++ Day25/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day25;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputFileName = args.Length == 0 ? "test.txt" : args[0];
+        Console.WriteLine("test");
+    }
+}
